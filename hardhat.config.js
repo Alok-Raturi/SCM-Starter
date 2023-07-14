68d7a14f33553507ce35d1587e399c21021bef0b
Alok-Raturi <110283609+Alok-Raturi@users.noreply.github.com>
2023-07-14 10:17:53 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,9 +5,9 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/WWejNSlVRE1FNZY-btzqa3RkTDkT787s",
+      url: "<Your Alchemy URL>",
       accounts: [
-        "dbbc20a226fc60e80c203331ef1a960cdb4aad60f6608df10535e79716a75abe",
+        "<Your Accounts private key>",
       ],
     },
   },
